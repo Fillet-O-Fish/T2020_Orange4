70e861fe9aa988ab1899ae4d799d43f02c2587e2
Zyle <inu99scar@gmail.com>
2019-10-31 12:08:16 +0800

    added django

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'r6sh@oflaad#cvm9%nj2s0iahla=u20rgbwd!hz)3)j5672y%@'
+SECRET_KEY = 'oob!&sr3c)cg39qvij(_h$!-83^4=ocoy!3^-3%+zrh+7m62v*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
