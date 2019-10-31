2c3379d42ad953a3930792340f050e76ca0cd270
Zyle <inu99scar@gmail.com>
2019-10-31 10:48:08 +0800

    added get functions

--- test.py
+++ test.py
@@ -1,8 +1,23 @@
 import requests
 
 URL_STR = "http://techtrek-api-gateway.ap-southeast-1.elasticbeanstalk.com"
-USER = "limzeyang"
 headers = {'identity': 'Group13', 'token' : '9d14bdf1-ffbe-48ef-b80a-99e9c85eaa22'}
-url = URL_STR + "/customers/" + USER
-r = requests.get(url,headers=headers)
-print (r.text)
+
+def getFunction(url):
+    r = requests.get(url, headers=headers)
+    return r.json()
+
+def getCustIdUrl(userName):
+    url = URL_STR + "/customers/" + userName
+    getFunction(url)
+
+def getCustDetailsUrl(custId):
+    url = URL_STR + "/customers/" + custId + "/details/"
+    getFunction(url)
+
+
+def getTransactionDetailsUrl(accId, fromDate, toDate):
+    url = URL_STR + "/transaction/" + accId + "?from=" + fromDate + "&to=" + toDate
+    getFunction(url)
+
+
