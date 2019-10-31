1ffd972ef04242fb4d0034d3911bc5d447c903e0
Zyle <inu99scar@gmail.com>
2019-10-31 11:05:39 +0800

    updated functions

--- test.py
+++ test.py
@@ -3,27 +3,30 @@ import requests
 URL_STR = "http://techtrek-api-gateway.ap-southeast-1.elasticbeanstalk.com"
 headers = {'identity': 'Group13', 'token' : '9d14bdf1-ffbe-48ef-b80a-99e9c85eaa22'}
 
-def getFunction(url):
+def getCustIdUrl(userName):
+    url = URL_STR + "/customers/" + userName
     r = requests.get(url, headers=headers)
     return r.json()
 
-def getCustIdUrl(userName):
-    url = URL_STR + "/customers/" + userName
-    getFunction(url)
-
 def getCustDetailsUrl(custId):
     url = URL_STR + "/customers/" + custId + "/details/"
-    getFunction(url)
+    r = requests.get(url, headers=headers)
+    return r.json()
 
 
 def getTransactionDetailsUrl(accId, fromDate, toDate):
     url = URL_STR + "/transaction/" + accId + "?from=" + fromDate + "&to=" + toDate
-    getFunction(url)
+    r = requests.get(url, headers=headers)
+    return r.json()
 
 def getPersonalMsg(custId):
     url = URL_STR + "/message/" + custId
-    getFunction(url)
+    r = requests.get(url, headers=headers)
+    return r.json()
 
 def getMarketMsg(custId):
     url = URL_STR + "/marketing/" + custId
-    getFunction(url)
+    r = requests.get(url, headers=headers)
+    return r.json()
+
+print(getCustIdUrl("limzeyang"))
